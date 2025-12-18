0476fe2eadb5126acca1086573365e41d52645c0
trava <nicola1.travaglini@gmail.com>
2025-12-18 09:34:27 +0100

    add: portability constants

--- syscall_handler/syscall_handler.h
+++ syscall_handler/syscall_handler.h
@@ -3,26 +3,39 @@
 
 #define FUSE_USE_VERSION 30
 
+// Portability constants
+#ifndef FUSE_FILL_DIR_DEFAULTS
+#define FUSE_FILL_DIR_DEFAULTS 0
+#endif
+
 #include <fuse3/fuse.h>
 
 #include "../db_handler/db_handler.h"
 
-#define COUNT_CHAR(str, ch) ({ \
-    int count = 0; \
-    for (int i = 0; str[i] != '\0'; i++) { \
-        if (str[i] == ch) count++; \
-    } \
-    count; \
-})
+#define COUNT_CHAR(str, ch)                                                    \
+  ({                                                                           \
+    int count = 0;                                                             \
+    for (int i = 0; str[i] != '\0'; i++) {                                     \
+      if (str[i] == ch)                                                        \
+        count++;                                                               \
+    }                                                                          \
+    count;                                                                     \
+  })
 
 void vfs2db_destroy(void *private_data);
 
-int  vfs2db_getattr(const char *path, struct stat *st, struct fuse_file_info *fi);
-int  vfs2db_getxattr(const char *path, const char *name, char *value, size_t size);
-int  vfs2db_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags);
-int  vfs2db_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi);
-int  vfs2db_write(const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *fi);
-int  vfs2db_create(const char* path, mode_t mode, struct fuse_file_info *fi);
-int  vfs2db_readlink(const char* path, char* buffer, size_t size);
+int vfs2db_getattr(const char *path, struct stat *st,
+                   struct fuse_file_info *fi);
+int vfs2db_getxattr(const char *path, const char *name, char *value,
+                    size_t size);
+int vfs2db_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
+                   off_t offset, struct fuse_file_info *fi,
+                   enum fuse_readdir_flags flags);
+int vfs2db_read(const char *path, char *buffer, size_t size, off_t offset,
+                struct fuse_file_info *fi);
+int vfs2db_write(const char *path, const char *buffer, size_t size,
+                 off_t offset, struct fuse_file_info *fi);
+int vfs2db_create(const char *path, mode_t mode, struct fuse_file_info *fi);
+int vfs2db_readlink(const char *path, char *buffer, size_t size);
 
 #endif // SYSCALL_HANDLER_H
